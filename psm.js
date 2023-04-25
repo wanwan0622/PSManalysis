62098a0f2444bf903cce3f32efa41362d6128d77
bamboo <minnie20000622@yahoo.co.jp>
2023-04-25 22:54:38 +0900

    データの整形

--- psm.js
+++ psm.js
@@ -3,9 +3,19 @@ function main(input) {
     const nums = args.map((n) => n.split(","));
   //   console.log(nums);
     const headers = nums[0];      // csvファイルのヘッダー
-    console.log(headers);
+  //   console.log(headers);
     const num_sample = nums.length - 2;   // サンプル数
-    console.log(num_sample);
+  //   console.log(num_sample);
+    const dict_data = [];
+    for (let i=1; i<=num_sample; i++) {
+        let tmp_dict = new Array();
+        for (let j=0; j<headers.length; j++) {
+            tmp_dict[headers[j]] = nums[i][j];
+        }
+        dict_data.push(tmp_dict);
+    }
+    console.log(dict_data);
   }
   
   main(require("fs").readFileSync("/dev/stdin", "utf8"));
+  
