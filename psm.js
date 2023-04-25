f7305cc9bcb7e1359e8bad27adabd9bf34442301
bamboo <minnie20000622@yahoo.co.jp>
2023-04-25 23:49:09 +0900

    データの中の最大値を求める

--- psm.js
+++ psm.js
@@ -1,4 +1,5 @@
 function main(input) {
+    // データの入力受け取り
     const args = input.split("\n");
     const nums = args.map((n) => n.split(","));
     // console.log(nums);
@@ -6,14 +7,22 @@ function main(input) {
     // console.log(headers);
     const num_sample = nums.length - 2;   // サンプル数
     // console.log(num_sample);
+
+    // データの整形
     const dict_data = [];
+    let max_price = 0;    // csvに書かれている金額の最大値
     for (let i=1; i<=num_sample; i++) {
         let tmp_dict = new Array();
         for (let j=0; j<headers.length; j++) {
-            tmp_dict[headers[j]] = nums[i][j];
+            let num = Number(nums[i][j]);
+            tmp_dict[headers[j]] = num;
+            if (j !== 0 && num >= max_price) {
+                max_price = num;
+            }
         }
         dict_data.push(tmp_dict);
     }
+    console.log(max_price);
     console.log(dict_data);
 }
 
