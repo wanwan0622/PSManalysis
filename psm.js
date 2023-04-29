3840d3fc169c5eb79970a91764431c0f837fdeac
bamboo <minnie20000622@yahoo.co.jp>
2023-04-29 15:44:52 +0900

    割合を求める

--- psm.js
+++ psm.js
@@ -20,10 +20,32 @@ function main(input) {
                 max_price = num;
             }
         }
-        dict_data.push(tmp_dict);
+        if (tmp_dict !== undefined) dict_data.push(tmp_dict);
     }
-    console.log(max_price);
-    console.log(dict_data);
+    // console.log(max_price);
+    // console.log(dict_data);
+    // console.log(dict_data[0]["高い"]);
+    
+    // 割合を求める
+    const diff = 50;     // 何円単位で集計するか
+    const rate_data = new Array();
+    for (let price=diff; price<=max_price; price+=diff) {
+        let tmp_rate = new Array();
+        for (let i=1; i<=4; i++) {
+            tmp_rate[headers[i]] = 0;
+        }
+        for (let i=0; i<num_sample; i++) {
+            if (dict_data[i]["高い"] <= price) tmp_rate["高い"]++;
+            if (dict_data[i]["安い"] >= price) tmp_rate["安い"]++;
+            if (dict_data[i]["高すぎる"] <= price) tmp_rate["高すぎる"]++;
+            if (dict_data[i]["安すぎる"] >= price) tmp_rate["安すぎる"]++;
+        }
+        for (let i=1; i<=4; i++) {
+            tmp_rate[headers[i]] /= (num_sample / 100);
+        }
+        rate_data[price] = tmp_rate;
+    }
+    console.log(rate_data);
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
