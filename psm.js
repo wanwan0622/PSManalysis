655f3e6b12b3c302075e57463ae7aba01f252d60
bamboo <minnie20000622@yahoo.co.jp>
2023-04-29 19:08:06 +0900

    グラフの交点を求める関数を追加

--- psm.js
+++ psm.js
@@ -1,3 +1,44 @@
+const assert = require("assert");
+
+/**
+ * header1とheader2のグラフの交点の座標を求める関数
+ * @param {dict} rateData 割合が入った辞書
+ * @param {string} header1 安い、安すぎるのいずれか
+ * @param {string} header2 高い、高すぎるのいずれかで、header1で選んでいないもの
+ * @param {number} diff 何円単位で集計したか。x座標のステップ数
+ * @return {number | null} x 交点のx座標
+ * @return {number | null} y 交点のy座標
+**/
+function getIntersection(rateData, header1, header2, diff) {
+    if (!(header1 === "安い" || header1 === "安すぎる")) return [null, null];
+    if (!(header2 === "高い" || header2 === "高すぎる")) return [null, null];
+    
+    let intersection = -1;
+    for (let i=0; i<rateData.length; i++) {
+        if (rateData[i][header1] <= rateData[i][header2]) {
+            intersection = i;
+            break;
+        }
+    }
+    console.log(intersection);
+    assert.notStrictEqual(intersection, -1);    // 絶対に交わるので
+    const x1 = (intersection - 1) * diff;
+    const x3 = x1;
+    const x2 = (intersection) * diff;
+    const x4 = x2;
+    const y3 = rateData[intersection-1][header1];
+    const y1 = rateData[intersection-1][header2];
+    const y4 = rateData[intersection][header1];
+    const y2 = rateData[intersection][header2];
+    // const x1 = 0; const x2 = 2; const x3 = 0; const x4 = 2;
+    // const y1 = 0; const y2 = 2; const y3 = 2; const y4 = 0
+    
+    const x = (((y3 - y1) * (x1 - x2) * (x3 - x4)) + (x1 * (y1 - y2) * (x3 - x4)) - (x1 * (y3 - y4) * (x1 - x2))) / (((y1 - y2) * (x3 - x4)) - ((x1 - x2) * (y3 - y4)));
+    const y = (x * (y1 - y2) / (x1 - x2)) + y1 - (x1 * ((y1 - y2) / (x1 - x2)));
+    console.log(x, y);
+    return [x, y];
+}
+
 function main(input) {
     // データの入力受け取り
     const args = input.split("\n");
