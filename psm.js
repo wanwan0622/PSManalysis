4a83387fb9f0c3bdf768f50e7acf3723e9a95881
bamboo <minnie20000622@yahoo.co.jp>
2023-04-29 19:17:47 +0900

    コメントの追加と不要な行の削除

--- psm.js
+++ psm.js
@@ -10,18 +10,22 @@ const assert = require("assert");
  * @return {number | null} y 交点のy座標
 **/
 function getIntersection(rateData, header1, header2, diff) {
+    // 関数の呼び出しのタイミングで引数がおかしいときは、nullを返す
     if (!(header1 === "安い" || header1 === "安すぎる")) return [null, null];
     if (!(header2 === "高い" || header2 === "高すぎる")) return [null, null];
     
     let intersection = -1;
     for (let i=0; i<rateData.length; i++) {
+        // y座標の大小関係が反転するタイミングを見つける
         if (rateData[i][header1] <= rateData[i][header2]) {
             intersection = i;
             break;
         }
     }
-    console.log(intersection);
-    assert.notStrictEqual(intersection, -1);    // 絶対に交わるので
+    // 必ずグラフの交点は存在するので、intersectionが-1から更新されていない場合はエラーを出す
+    assert.notStrictEqual(intersection, -1);
+
+    // PDF9枚目の図と同じ座標の関係
     const x1 = (intersection - 1) * diff;
     const x3 = x1;
     const x2 = (intersection) * diff;
@@ -30,12 +34,11 @@ function getIntersection(rateData, header1, header2, diff) {
     const y1 = rateData[intersection-1][header2];
     const y4 = rateData[intersection][header1];
     const y2 = rateData[intersection][header2];
-    // const x1 = 0; const x2 = 2; const x3 = 0; const x4 = 2;
-    // const y1 = 0; const y2 = 2; const y3 = 2; const y4 = 0
     
+    // 交点の座標
     const x = (((y3 - y1) * (x1 - x2) * (x3 - x4)) + (x1 * (y1 - y2) * (x3 - x4)) - (x1 * (y3 - y4) * (x1 - x2))) / (((y1 - y2) * (x3 - x4)) - ((x1 - x2) * (y3 - y4)));
     const y = (x * (y1 - y2) / (x1 - x2)) + y1 - (x1 * ((y1 - y2) / (x1 - x2)));
-    console.log(x, y);
+
     return [x, y];
 }
 
@@ -43,11 +46,8 @@ function main(input) {
     // データの入力受け取り
     const args = input.split("\n");
     const nums = args.map((n) => n.split(","));
-    // console.log(nums);
     const headers = nums[0];      // csvファイルのヘッダー
-    // console.log(headers);
     const numSample = nums.length - 2;   // サンプル数
-    // console.log(numSample);
 
     // データの整形
     const dictData = [];
@@ -63,12 +63,9 @@ function main(input) {
         }
         if (tmpDict !== undefined) dictData.push(tmpDict);
     }
-    // console.log(maxPrice);
-    // console.log(dictData);
-    // console.log(dictData[0]["高い"]);
     
     // 割合を求める
-    const diff = 50;     // 何円単位で集計するか
+    const diff = 50;     // 何円単位で集計するか(ここを変えると結果の正確さが変わる)
     const rateData = [];
     for (let price=diff; price<=maxPrice; price+=diff) {
         let tmpRate = new Array();
@@ -86,7 +83,8 @@ function main(input) {
         }
         rateData.push(tmpRate);
     }
-    // console.log(rateData);
+
+    // 出力(小数第1位で四捨五入)
     const saiko = getIntersection(rateData, "安い", "高すぎる", diff);
     console.log("最高価格　　　　", Math.round(saiko[0]).toString()+"円");
     const dakyo = getIntersection(rateData, "安い", "高い", diff);
